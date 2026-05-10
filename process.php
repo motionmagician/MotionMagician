c34c5ffa0f251c9b8c86724f1b519ac5c759f5f4
MotionMagician <motionmagician1@gmail.com>
2026-05-11 00:05:59 +0530

    fresh start

--- process.php
+++ process.php
@@ -3,17 +3,21 @@ $conn = mysqli_connect("localhost", "root", "", "userdata");
 
 if (isset($_POST['submit'])) {
     $username = $_POST['email'];
+    $extract = substr($username, -10);
+    $check = "@gmail.com";
     $pass = $_POST['password'];
 
-    $sql = "INSERT INTO users (name, password) VALUES ('$username', '$pass')";
-     if (mysqli_query($conn, $sql)) {
-
-        // ✅ REDIRECT HERE
-        header("Location: success.php");
+    if ($extract === $check) {
+        $sql = "INSERT INTO users (name, password) VALUES ('$username', '$pass')";
+        if (mysqli_query($conn, $sql)) {
+            header("Location: success.php");
+            exit();
+        }
+    }else {
+        echo "<h1>Invalid email address. Please use a Gmail account.</h1>";
         exit();
+    }
 }
-}
-
 ?>
 
 
